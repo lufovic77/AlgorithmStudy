548905c398ed36298f445a6c77126d601d75cffe
lufovic77 <lufovic77@gmail.com>
2020-03-15 10:52:18 +0000

    primitive of 1783

--- greedy/1783.cpp
+++ greedy/1783.cpp
@@ -1,45 +1,48 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
 using namespace std;
-//어차피 세로 길이를 두배로 늘리면 위로만 가는 케이스랑 다를바가 없다. 
-typedef unsigned long long ll;
+int N, M;
+int moveX[4] = {-2, 1, -1, 2};
+int moveY[4] = { 1, 2,  2, 1};
+int chess(int posX, int posY, vector <bool> &flag,int count, int type){
+	int ans = -1;
+	int fail = false;
+	for(int i=0;i<4;i++){
+		int newX = posX+moveX[i];
+		int newY = posY+moveY[i];
+		if(newX <M && newY<N && newX>=0 && newY>0){
+			flag[i] = true;
+			ans = max(ans, chess(newX, newY, flag, count+1, i));
+			fail = true;
+		}
+	}
+	if(fail == false){
+		if(count>=4){
+			for(int i=0;i<4;i++){
+				if(flag[i] == false){
+					return -1;
+				}			
+			}
+			return count;
+		}
+		else{
+			return count;		
+		}
+	}	
+	return ans;
+}
 int main(){
-    ll n, m;
-    cin>>n>>m;
-
-    ll total = 0;
-    ll flag = 0;
-    if(m<=2 && n<=2){
-        cout <<"1";
-        return 0;
-    }
-    n *= 2;
-    if(n>=6 && m>=6){
-        n -= 6;
-        m -= 6;
-        total = 4; 
-        flag = 1;
-    }
-    if(n>=m){
-        n /= 2;
-        if(n>m){
-            total += m;
-        }
-        else{
-            total+=n;
-        }
-        if(total>4 && flag == 0)
-        total = 4;
-    }
-    else{
-        m /= 2;
-        if(m>n){
-            total += n;
-        }
-        else{
-            total+=m;
-        }
-        if(total>4 && flag == 0)
-        total = 4;
-    } 
-    cout<<total;
+	cin>>M>>N;
+	vector <bool> flag;
+	for(int i=0;i<4;i++)
+		flag.push_back(false);
+	int curX = M-1;
+	int curY = 0;
+	int	ans = max(ans, chess(curX ,curY, flag, 1, -1));
+	if (ans ==-1)
+		cout<<"1";
+	else
+		cout<<ans;
 }
