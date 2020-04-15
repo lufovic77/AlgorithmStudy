2b3b472a03e1a88c8ed22bdc1f4ac584c0d32742
lufovic77 <lufovic77@gmail.com>
2020-04-15 06:18:08 +0000

    simple usage of vector

---
+++ STL/11931.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+using namespace std;
+bool CMP(int a, int b){return a>=b;};
+int main(){
+	int N,tmp;
+	scanf("%d", &N);
+	vector<int> arr;
+	for(int i=0;i<N;i++){
+		scanf("%d", &tmp);
+		arr.push_back(tmp);
+	}
+	sort(arr.begin(), arr.end(), CMP);
+	for(auto ele: arr)
+		printf("%d\n", ele);
+}
