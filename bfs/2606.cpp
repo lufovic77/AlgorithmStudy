1308f08b50f9961c0ac7de724e7255a8d522c184
lufovic77 <lufovic77@gmail.com>
2020-06-28 11:10:26 +0000

    modified in pair number

--- bfs/2606.cpp
+++ bfs/2606.cpp
@@ -5,26 +5,28 @@
 using namespace std;
 int main(){
 	int n, pair, t1, t2, cnt, cur;
-	int visited[11];
-	fill(visited, visited+10, 0);
-	vector<int> map[11];
+	int visited[101];
+	fill(visited, visited+100, 0);
+	vector<int> map[101];
 	cin>>n>>pair;
 	FOR(i, pair){
 		cin>>t1>>t2;
 		map[t1].push_back(t2);
+		map[t2].push_back(t1);
 	}
 	cnt = 0;
-	queue<int> q;
-	q.push(1);
-	while(!q.empty()){
-		cur = q.back();
-		q.pop();
-		if(visited[cur] == 1)
-			continue;
-		visited[cur] = 1;		
-		for(auto a: map[cur])
-			q.push(a);
-		cnt++;
-	}
+		queue<int> q;
+		q.push(1);
+		while(!q.empty()){
+			cur = q.front();
+			q.pop();
+			if(visited[cur] == 1)
+				continue;
+			visited[cur] = 1;		
+			for(auto a: map[cur])
+				q.push(a);
+			if(cur != 1)
+			cnt++;
+		}
 	cout<<cnt;
 }
