7ac75d2facfaddfa43290a27539bdfdf75a9adf9
lufovic77 <lufovic77@gmail.com>
2020-06-30 11:17:38 +0000

    utilizing map STL

---
+++ STL/7785_revision.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <cstdio>
+#include <stack>
+#define FOR(i,n) for(int i=0;i<n;i++)
+using namespace std;
+int main(){
+	string name, stat;
+	int n;
+	cin>>n;
+	map<string, bool> m;
+	FOR(i,n){
+		cin>>name>>stat;
+		if(stat == "enter")
+			m.insert(make_pair(name, true));
+		else
+			m.erase(name);
+	}
+	stack <string> s;
+	for(auto it = m.begin(); it!=m.end() ;it++ )
+		s.push(it->first);
+	while(!s.empty()){
+		cout<<s.top()<<endl;
+		s.pop();
+	}
+}
