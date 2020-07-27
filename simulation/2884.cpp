82754eb06e0b7a7e64a13378d3fbf13968b84eee
lufovic77 <lufovic77@gmail.com>
2020-07-27 10:57:20 +0000

    revision

--- simulation/2884.cpp
+++ simulation/2884.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <cstdio>
 using namespace std;
+/*
+ * Revision*/
+int main(){
+	int h,m,rem;
+	cin>>h>>m;
+	if(m>=45)
+		printf("%d %d", h, m-45);
+	else{
+		rem = 45-m;
+		rem = 60-rem;
+		if(h == 0)
+			printf("23 %d",rem);
+		else
+			printf("%d %d", h-1, rem);
+	}
+}
 
+/* 
 int main(){
 	int H,M;
 	cin>>H>>M;
@@ -16,3 +34,4 @@ int main(){
 		cout<<minute/60<<" "<<minute%60;
 	}
 }
+*/
