f59c1c3434655d70dd2a251f429ce4dbd8c56f72
lufovic77 <lufovic77@gmail.com>
2020-08-08 04:46:36 +0000

    hash to set

--- simulation/1764.cpp
+++ simulation/1764.cpp
@@ -2,30 +2,22 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <set>
 using namespace std;
-unsigned long hashStr(string str){
-	unsigned long hash = 5381;
-	int c, i;
-	i = 0;
-	while (c = str[i++])
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	return hash%700000;
-}
-int arr[700000];
 int main(){
 	int n, m;
 	cin>>n>>m;
 	string tmp1, tmp2;
+	set <string> s;
 	for(int i=0;i<n;i++){
 		cin>>tmp1;
-		arr[hashStr(tmp1)] = 1;
+		s.insert(tmp1);
 	}
-	int index;
 	vector <string> ret;
 	for(int i=0;i<m;i++){
 		cin>>tmp2;
-		index = hashStr(tmp2);
-		if (arr[index]==1)
+		auto a = s.find(tmp2);
+		if (a!=s.end())
 			ret.push_back(tmp2);
 	}
 	sort(ret.begin(), ret.end());
