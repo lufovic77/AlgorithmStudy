11d7249683c39818aa361334eca21bb201e13f34
lufovic77 <lufovic77@gmail.com>
2020-09-13 07:24:27 +0000

    index starting from 1

--- segmentTree/10868.cpp
+++ segmentTree/10868.cpp
@@ -7,28 +7,26 @@
 using namespace std;
 class segT{
 	private:
-		int tree[400000];
+		int tree[300000];
 	public:
 		segT(vector<int> arr, int n){
-			init(arr, 1, 0, n-1);
+			init(arr, 1, 1, n);
 		}
 		int init(vector<int> arr, int node, int left, int right){
 			if(left == right){
 				return tree[node] = arr[left];
 			}
 			int mid = (left+right)/2;
-			return tree[node] = min(\
+			return tree[node] = min(
 					init(arr, node*2, left, mid), init(arr, node*2+1, mid+1, right));
 		}
 		int query(int l, int r, int node, int leftN, int rightN){
-			if(leftN==rightN)
-				return tree[node];
 			if(rightN<l || leftN>r)
 				return INT_MAX;
-			if(l<=leftN && r<=rightN)
+			if(l<=leftN && rightN<=r)
 				return tree[node];
-			int mid = (leftN+rightN);
-			return min(query(l, r, node*2, leftN, mid),\
+			int mid = (leftN+rightN)/2;
+			return min(query(l, r, node*2, leftN, mid),
 					query(l, r, node*2+1, mid+1, rightN));
 		}
 };
@@ -36,6 +34,7 @@ int main(){
 	int n, m, tmp, a, b, ans;
 	cin>>n>>m;
 	vector <int> arr;
+	arr.push_back(0);
 	FOR(i, n){
 		cin>>tmp;
 		arr.push_back(tmp);
@@ -44,7 +43,7 @@ int main(){
 	FOR(i, m){
 		cin>>a>>b;
 		//Do something
-		ans = obj.query(a, b, 1, 0, n-1);
+		ans = obj.query(a, b, 1, 1, n);
 		printf("%d\n", ans);
 	}
 }
