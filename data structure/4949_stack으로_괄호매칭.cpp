53be09141b3517e40c98cf0271e8c42829372e25
김경민 <lufovic77@gmail.com>
2020-10-15 11:17:39 +0000

    add conditions

--- data structure/4949_stack으로_괄호매칭.cpp
+++ data structure/4949_stack으로_괄호매칭.cpp
@@ -10,58 +10,49 @@ using namespace std;
  * 입력함수의 교훈
  * 1. getline(cin, string): 띄어쓰기도 포용하는 넓은 친구이지만, 엔터(\n)도 입력으로 받아버린다. 
  * 2. scanf: 모두가 알듯이 띄어쓰기가 나오면 입력을 멈춘다. 
- * 3. gets: getline과 scanf의 가운데. 공백도 포용하며, 엔터도 생략해서 받는다. i
+ * 3. gets: getline과 scanf의 가운데. 공백도 포용하며, 엔터도 생략해서 받는다. 
+ * ps
+ * "" -> string
+ * '' -> char
  * */
 int main(){
 	bool flag;
+	int alter=0;
 	while(1){
+		alter++;
 		bool flag = false;
 		string input;
 		stack <char>st;
 		getline(cin, input);
-		if(input[0]=='.')
+		if(input==".")
 			return 0;
+		if(alter%2==0)
+			continue;
 		FOR(i, input.length()){
-			flag = true;
 			if(input[i]=='(' || input[i]=='['){
 				st.push(input[i]);
 				continue;
-			}
-			else if (input[i]==')'){
+			}	
+			if(input[i]==')'){
 				if(st.empty()){
-					printf("no\n");
-					st.push('f');
+					flag = true;
 					break;
 				}
-				if(st.top()=='('){
+				if(st.top()=='(')
 					st.pop();
-					continue;
-				}
-				else{
-					printf("no\n");
-					break;
-				}
 			}
-			else if(input[i]==']'){
+			if(input[i]==']'){
 				if(st.empty()){
-					printf("no\n");
-					st.push('f');
+					flag = true;
 					break;
 				}
-				if(st.top()=='['){
+				if(st.top()=='[')
 					st.pop();
-					continue;
-				}
-				else{
-					printf("no\n");
-					break;
-				}
 			}
-			else
-				continue;
 		}
-		if(flag&&st.empty())
+		if(!st.empty() || flag)
+			printf("no\n");
+		else if(!flag && st.empty())
 			printf("yes\n");
-			
 	}
 }
