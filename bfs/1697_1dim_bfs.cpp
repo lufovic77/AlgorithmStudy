9ce6f44c255b0427f9f1c24cde3270aaf2cce511
김경민 <lufovic77@gmail.com>
2020-10-24 05:48:06 +0000

    1-dimensional bfs

---
+++ bfs/1697_1dim_bfs.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+int visited[100001];
+int main(){
+	int n, m, ret=0;
+	cin>>n>>m;
+	if(m<=n){
+		while(n!=m){
+			n--;
+			ret++;
+		}
+		cout<<ret;
+		return 0;
+	}
+	while(2*n<=m){ visited[n]=1; n*=2;ret++;}
+	queue <pair<int, int> > q;
+	q.push(make_pair(n, ret));
+	while(!q.empty()){
+		n = q.front().first;
+		ret = q.front().second;
+		q.pop();
+		if(n>100000)
+			continue;
+		if(visited[n])
+			continue;
+		visited[n] = 1;
+		if(n==m){
+			cout<<ret;
+			return 0;
+		}
+		q.push(make_pair(n-1,ret+1));
+		q.push(make_pair(n+1,ret+1));
+		q.push(make_pair(n*2,ret+1));
+	}
+}
