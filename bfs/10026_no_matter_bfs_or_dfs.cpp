79798cc4596932a997a96a3c10c54cac84dc1ae4
김경민 <lufovic77@gmail.com>
2020-10-24 06:43:31 +0000

    typical bfs

---
+++ bfs/10026_no_matter_bfs_or_dfs.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#define FOR(i, n) for(int i=0;i<n;i++)
+using namespace std;
+int normal[101][101];
+int vn[101][101];
+int disable[101][101];
+int vd[101][101];
+/*
+ * R for 0
+ * G for 1
+ * B for 2
+ * */
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, 1, 0, -1};
+int main(){
+	int n;
+	char c;
+	cin>>n;
+	FOR(i, n){
+		FOR(j, n){
+			cin>>c;
+			switch(c){
+				case 'R':
+					break;
+				case 'G':
+					normal[i][j] = 1;
+					break;
+				case 'B':
+					normal[i][j] = 2;
+					disable[i][j] = 2;
+					break;
+				default:
+					continue;
+			}
+
+		}
+	}
+	queue<pair<int, int> > q;
+	int x, y, dist = 0, tmp, cnt=0;
+	//for normal
+	FOR(i, n){
+		FOR(j, n){
+			if(vn[i][j]!=1){
+				q.push(make_pair(i, j));
+				tmp = normal[i][j];
+				while(!q.empty()){
+					x = q.front().first;
+					y = q.front().second;
+					vn[x][y] = 1;
+					q.pop();
+					FOR(k, 4){
+						int nx = x+dx[k], ny = y+dy[k];
+						if(vn[nx][ny])
+							continue;
+						if(nx<0||nx>=n||ny<0||ny>=n)
+							continue;
+						if(normal[nx][ny] != tmp)
+							continue;
+						q.push(make_pair(nx, ny));
+					}
+				}
+				dist++;
+			}
+		}
+	}
+	cout<<dist<<" ";
+	dist = 0;
+	while(!q.empty())
+		q.pop();
+	//for disabled. precisely, they are not disabled. 
+	FOR(i, n){
+		FOR(j, n){
+			if(vd[i][j]!=1){
+				q.push(make_pair(i, j));
+				tmp = disable[i][j];
+				while(!q.empty()){
+					x = q.front().first;
+					y = q.front().second;
+					vd[x][y] = 1;
+					q.pop();
+					FOR(k, 4){
+						int nx = x+dx[k], ny = y+dy[k];
+						if(vd[nx][ny])
+							continue;
+						if(nx<0||nx>=n||ny<0||ny>=n)
+							continue;
+						if(disable[nx][ny] != tmp)
+							continue;
+						q.push(make_pair(nx, ny));
+					}
+				}
+				dist++;
+			}
+		}
+	}
+	cout<<dist;
+}
