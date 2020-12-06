af6753a56373376413256cb6198d08a382f78584
lufovic77 <lufovic77@gmail.com>
2020-12-06 06:09:36 +0000

    last attempt

--- binarySearch/2110_recursionToLoop.cpp
+++ binarySearch/2110_recursionToLoop.cpp
@@ -15,29 +15,25 @@ int main(){
 		house.push_back(tmp);
 	}
 	sort(house.begin(), house.end());
-	int l = 1, r = house[house.size()-1], mid, prev, cnt, mini;
+	int l = 1, r = house[house.size()-1], mid, prev, cnt, ret;
 	while(l<=r){
 		mid = (l+r)/2;
-		mini = INT_MAX;
 		prev = 0;
 		cnt = 1;
 		for(int i=1;i<house.size();i++){
 			tmp = house[i] - house[prev];
 			if(tmp >= mid){
 				prev = i;
-				mini = min(mini, tmp);
 				cnt++; //pick house to launch device
 			}
 		}
 		if(cnt<c)
 			r = mid-1;
-		else if (cnt > c)
+		else{	
+			ret = mid;
 			l = mid+1;
-		else{ //exactly the case
-			cout<<mini;
-			return 0;
 		}
 	}		
-	cout<<mini;
+	cout<<ret;
 }
 
