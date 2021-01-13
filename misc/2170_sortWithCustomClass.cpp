cdfcb8c4098c8625143094741dcce5f07dc4d899
김경민 <lufovic77@gmail.com>
2021-01-13 11:09:04 +0000

    DONE solving

--- misc/2170_sortWithCustomClass.cpp
+++ misc/2170_sortWithCustomClass.cpp
@@ -10,15 +10,35 @@ using namespace std;
 bool cmp(pair<int, int> u, pair<int, int> t){
 	if(u.first==t.first)
 		return u.second <t.second;
-	return u.second<=t.second;
+	return u.first<t.first;
 }
 
 int main(){
 	int n, a, b;
 	vector<pair<int, int> >pos;
+	cin>>n;
 	FOR(i, n){
 		scanf("%d%d", &a, &b);
 		pos.push_back(make_pair(a, b));
 	}
 	sort(pos.begin(), pos.end(), cmp);
+	int len = 0,l, r;
+	FOR(i, n){
+		int f = pos[i].first;
+		int s = pos[i].second;
+		if(i == 0){
+			l = f, r = s;
+			continue;
+		}
+		if(f > r){ // new range		
+			len += (r-l);
+			l = f, r=s;
+		}
+		if(f <= r && s>r){
+			r = s;
+		}
+		if(i==n-1)
+			len+=(r-l);
+	}
+	cout<<len;
 }
