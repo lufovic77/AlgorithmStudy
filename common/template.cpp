4cf80967e114e7fbae99e50245355177addba6f8
김경민 <lufovic77@gmail.com>
2021-02-14 05:39:02 +0000

    common template

--- common/template.cpp
+++ common/template.cpp
@@ -3,8 +3,11 @@
 #include <cmath>
 #include <vector>
 #include <algorithm>
+#include <limits.h>
+#include <queue>
 #define FOR(i,n) for(int i=0;i<n;i++)
+#define FOR2(i,n) for(int i=1;i<=n;i++)
 using namespace std;
 int main(){
-
+	
 }
