7e21909f5ae3175c0b2652cc021e2537c4e35580
김경민 <lufovic77@gmail.com>
2021-02-14 05:38:22 +0000

    template for bfs

---
+++ common/template_bfs.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <limits.h>
+#include <queue>
+#define FOR(i,n) for(int i=0;i<n;i++)
+#define FOR2(i,n) for(int i=1;i<=n;i++)
+using namespace std;
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, 1, 0, -1};
+int main(){
+	
+}
