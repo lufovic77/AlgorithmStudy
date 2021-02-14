e45b985a687b7dd6578b03a9374b5ac3b06d46c8
김경민 <lufovic77@gmail.com>
2021-02-14 06:40:54 +0000

    out of bound runtime error

--- tree/11437_finding_leastCommonAncestor.cpp
+++ tree/11437_finding_leastCommonAncestor.cpp
@@ -14,7 +14,7 @@ vector<int> edge[50010];
 int main(){
 	int n, m, a, b;
 	vector<int> ret;
-	vector<int> tree[10010];
+	vector<int> tree[50010];
 	cin>>n;
 	ios::sync_with_stdio(false);
 	depth[1] = 1; //node number 1's depth: 1
@@ -24,6 +24,8 @@ int main(){
 		tree[a].push_back(b);
 		tree[b].push_back(a);
 	}
+	/* 정말 주의할 점은 자식과 부모 정보가 주어지지 않은 노드도 먼저 나올 수 있다는 것!
+	 * */
 	queue<int> q;
 	q.push(1);
 	while(!q.empty()){
