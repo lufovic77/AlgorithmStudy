91d4b4f8c283bdcc8eca160f40f3ebb079717f4b
lufovic77 <lufovic77@gmail.com>
2021-04-29 19:49:32 +0900

    deleted

--- dynamic programming/1912.cpp
+++ dynamic programming/1912.cpp
@@ -3,34 +3,27 @@ using namespace std;
 int main(){
 	int n;
 	cin>>n;
-
+	
 	int dp[100001];
 	int num[100001];
 	for(int i=0;i<n;i++)
 	cin>>num[i];
-	if(n == 1){
-		cout<<num[0];
-		return 0;
-	}
-	if(num[0]<0){
-		dp[0] = 0;
-	}
-	else{
-		dp[0] = num[0];
-	}
-	int max = num[0];
-
+	int max = -1001;
+	dp[0] = num[0];
 	for(int i=1;i<n;i++){
 		
 		int temp = dp[i-1] + num[i];
-		if(num[i] > 0){
+		if(temp > num[i]){
 			dp[i] = temp;
-			if (temp > max)
-			max = temp;
 		}
 		else{
 			continue;
 		}
 	}
-	cout << max;
+	for(int i=0;i<n;i++){
+		cout<< dp[i]<<endl;
+		if(max < dp[i])
+		max = dp[i];
+	}
+	cout<<max;
 }
